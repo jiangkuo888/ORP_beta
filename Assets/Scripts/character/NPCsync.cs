47888c25dd95a186588a0293b1108ddd7cde0754
jiangkuo888 <jiangkuo888@gmail.com>
2014-02-28 14:50:53 +0800

    NPC sync done

---
+++ Assets/Scripts/character/NPCsync.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCsync : MonoBehaviour {
+
+
+	public string syncEvent;
+	public PlayMakerFSM myFSM;
+	// Use this for initialization
+	void Start () {
+	
+		PhotonView photonView = this.gameObject.GetPhotonView();
+		
+		
+		photonView.RPC ("syncNPCstate",PhotonTargets.OthersBuffered);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	 
+	
+	[RPC]
+	void syncNPCstate(){
+		print ("111");
+		myFSM.Fsm.Event(syncEvent);
+		
+	}
+}
