0691b7e0b95b5d5c5cc067727b1db025d5fec415
zhen yang <slamerzy@gmail.com>
2014-05-16 12:50:55 +0800

    deleted a unneccessary file

--- Assets/Game 3/Script/Javascript/Game3_Camera.js
+++
@@ -1,9 +0,0 @@
-public var target : Transform;	//The player
-public var smoothTime = 0.3f;	//Smooth Time
-private var velocity : Vector2;	//Velocity
-
-function  Update ()
-{
-	//Set the position
-	transform.position = Vector3(Mathf.SmoothDamp(transform.position.x, target.position.x, velocity.x, smoothTime),Mathf.SmoothDamp( transform.position.y, target.position.y, velocity.y, smoothTime),transform.position.z);
-}
